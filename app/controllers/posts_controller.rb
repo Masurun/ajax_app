bebaa73785c3c74d9b0b8d88549ee8161d662a12
Masurun <wataru.5756@gmail.com>
2021-01-15 17:25:55 +0900

    ajaxappの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,11 @@
 class PostsController < ApplicationController
   def index
-  @posts = Post.all
+  @posts = Post.all.order(id: "DESC")
   end
   
-  def new
-  end
   
   def create
     Post.create(content: params[:content])#今回は入力データをビューで使用しないため＠入らないインスタンス変数
+    redirect_to action: :index
   end
 end
